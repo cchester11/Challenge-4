7ec78b9fa1831c734eb8a232c3616f7215dbfa1e
Charles <cchester1@angelo.edu>
2021-03-12 23:14:45 -0600

    worked on the game function

---
+++ assets/javascript/play.js
@@ -0,0 +1,41 @@
+// Question array to be pulled by functions
+let questions = [
+  {
+    question: "What is the correct syntax for referring to an external script called 'xxx.js'?",
+    choice1: "<script name='xxx.js'>",
+    choice2: "<script for='xxx.js'>",
+    choice3: "<script src='xxx.js'>",
+    choice4: "<scripts></scripts>",
+    answer: 3,
+  },
+  {
+    question: "The external JavaScript file must contain the <script> tag",
+    choice1: "True",
+    choice2: "False",
+    answer: 2,
+  },
+  {
+    question: "Inside which HTML element do we put the Javascript?",
+    choice1: "<scripting>",
+    choice2: "<script>",
+    choice3: "<js>",
+    choice4: "<javascript",
+    answer: 2,
+  },
+  {
+    question: "How do you write a function in Javascript",
+    choice1: "function = myFunction()",
+    choice2: "function myFunction()",
+    choice3: "myFunction = function()",
+    choice4: "None of the above",
+    answer: 2,
+  },
+  {
+    question: 'What is the difference between "==" and "==="?',
+    choice1: "Both B & C",
+    choice2: "Both operators are the same",
+    choice3: '"==" checks only for equality in value whereas "===" is a stricter equality test',
+    choice4: "None of the above",
+    answer: 3,
+  }
+];
